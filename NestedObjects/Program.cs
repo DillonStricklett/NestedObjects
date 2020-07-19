c68f69a452f0c616d17b17b675f67de0cd8e315c
Dillon Stricklett <dillonstricklett@live.com>
2020-07-19 15:44:29 -0700

    Added test code

--- NestedObjects/Program.cs
+++ NestedObjects/Program.cs
@@ -10,6 +10,52 @@ namespace NestedObjects
     {
         static void Main(string[] args)
         {
+            Instructor currInst = new Instructor()
+            {
+                FullName = "Homer Simpson",
+                Email = "Homer.Simpson@cptc.edu"
+            };
+
+            Student s1 = new Student()
+            {
+                FirstName = "Jim",
+                LastName = "Halpert"
+            };
+
+            Student s2 = new Student()
+            {
+                FirstName = "Sasuke",
+                LastName = "Uchiha"
+            };
+
+            Student s3 = new Student()
+            {
+                FirstName = "Rock",
+                LastName = "Lee"
+            };
+
+            Course testCourse = new Course();
+            testCourse.Title = "CPW 212 - Adv .Net";
+            testCourse.Description = "More .NET than you already learned";
+            testCourse.Credits = 5;
+            testCourse.CourseInstructor = currInst;
+            testCourse.Roster = new List<Student>();
+
+            // Adding students to course
+            testCourse.Roster.Add(s1);
+            testCourse.Roster.Add(s2);
+            testCourse.Roster.Add(s3);
+
+            Console.WriteLine(testCourse.Title);
+            Console.WriteLine("Is taught by " + 
+                testCourse.CourseInstructor.FullName);
+
+            Console.WriteLine("Currently enrolled students: ");
+            foreach(Student s in testCourse.Roster)
+            {
+                Console.WriteLine($"{s.FirstName} {s.LastName}");
+            }
+            Console.ReadKey();
         }
     }
 }
